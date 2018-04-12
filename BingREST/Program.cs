5ff333c452dae0943641d055ac5f5242cb7d5040
Stephen Costanzo <stephen.costanzo@snhu.edu>
2018-04-12 10:54:30 -0700

    Add project files.

---
+++ BingREST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BingREST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
